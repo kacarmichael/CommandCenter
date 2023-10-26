e728d8af171e6b4906379eb2f5c9400510bc8e32
kacarmichael <kacarmichael72@gmail.com>
2023-10-25 19:59:21 -0500

    Moar Djangular

---
+++ Djangular-Tutorial/djangular_trivia_api/trivia/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from .models import Question
+from .serializers import QuestionSerializer
+
+# Create your views here.
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
